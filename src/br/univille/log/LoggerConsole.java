b51e8f69513d43978c506201073ef0edccf96adf
namaria <ann.m.esp@gmail.com>
2025-06-10 19:52:40 -0300

    test

--- src/br/univille/log/LoggerConsole.java
+++ src/br/univille/log/LoggerConsole.java
@@ -1,13 +1,16 @@
 package br.univille.log;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 class LoggerConsole implements Logger {
     public void log (Level level, String message){
+        ZonedDateTime datahoraZona = ZonedDateTime.now();
         if (level == level.DEBUG){
-            System.out.println("\u001b[32m"+ level +": \u001b[0m"+ message);
+            System.out.println("\u001b[32m"+ datahoraZona.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")) +" "+ level +": "+ message);
         } else if (level == level.WARNING){
-            System.out.println("\u001b[33m"+ level +": \u001b[0m"+ message);
+            System.out.println("\u001b[33m"+ datahoraZona.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")) +" "+ level +": "+ message);
         } else if (level == level.ERROR){
-            System.out.println("\u001b[31m"+ level +": \u001b[0m"+ message);
+            System.out.println("\u001b[31m"+ datahoraZona.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")) +" "+ level +": "+ message);
         }
     }
 }
--- src/br/univille/log/LoggerFile.java
+++ src/br/univille/log/LoggerFile.java
@@ -3,11 +3,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 class LoggerFile implements Logger {
     public void log (Level level, String message) {
+        ZonedDateTime datahoraZona = ZonedDateTime.now();
         String filename = "log.txt";
-        String mensagem = level +": "+ message +"\n";
+        String mensagem = datahoraZona.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")) +" "+ level +": "+ message +"\n";
         try {
             Files.write(Paths.get(filename), mensagem.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         } catch (IOException e) {
